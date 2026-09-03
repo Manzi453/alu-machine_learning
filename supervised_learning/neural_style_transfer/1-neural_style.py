ae9009609ee6eee5d44f9f93650b5754aa1a163a
Manzi453 <i.manziyamu@alustudent.com>
2026-09-03 13:43:54 +0200

    refactor NST class to suppress output during VGG19 model loading

--- supervised_learning/neural_style_transfer/1-neural_style.py
+++ supervised_learning/neural_style_transfer/1-neural_style.py
@@ -4,6 +4,9 @@ Defines class NST that performs tasks for neural style transfer
 """
 
 
+import contextlib
+import os
+
 import numpy as np
 import tensorflow as tf
 
@@ -134,8 +137,10 @@ class NST:
 
         Saves the model in the instance attribute model
         """
-        VGG19_model = tf.keras.applications.VGG19(include_top=False,
-                                                  weights='imagenet')
+        with open(os.devnull, 'w') as devnull, \
+                contextlib.redirect_stdout(devnull):
+            VGG19_model = tf.keras.applications.VGG19(include_top=False,
+                                                      weights='imagenet')
         VGG19_model.save("VGG19_base_model")
         custom_objects = {'MaxPooling2D': tf.keras.layers.AveragePooling2D}
 
